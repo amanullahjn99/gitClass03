aa1b2c60a5a8e8de8188befba32d4e9e2b6a7c71
amanullahjn99 <amanullahjn99@gmail.com>
2023-08-02 19:22:29 -0400

    the code that tells me the aviablity of the project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Boolean productAvailable=true;
+        if (productAvailable){
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+        }
+    }
